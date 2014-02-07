035a08579d86ff6bd39e89d33c696bf09a3aeb03
HadiM <marelo64@gmail.com>
2014-02-07 11:31:44 +0100

    update doc/api_example.py

--- doc/api_example.py
+++ doc/api_example.py
@@ -1,7 +1,11 @@
 from lap_tracker.detection import PeakDetector
 from lap_tracker.tracker import Tracker
+from lap_tracker.io import TiffFile
 
-peaks_detector = PeakDetector('my_awesome_ome_file.tif')
+fname = 'my_awesome_ome_file.tif'
+tf = TiffFile(fname)
+
+peaks_detector = PeakDetector(tf or fname)
 peaks_detector.run(paralell=True)
 
 tracker = Tracker(peaks_detector.peaks)
